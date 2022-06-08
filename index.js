a1e10a9fee96bd0d56d05ec0c54603bf6393774e
alextaylor <alextaylor515@gmail.com>
2022-06-08 10:05:16 -0400

    commit tree 968f7ff2f3d43820eec2b7d9054cbdf272d3ca86

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+   let test = fetch("https://anapioficeandfire.com/api/books")
+  .then((resp)=>resp.json())
+  .then(renderBooks) 
+  return test
 }
-
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
